1272b8655e71c7f9bf1ea1098b6f4eae0c2c5ef6
mars <mars.maya.g@gmail.com>
2017-02-15 23:01:24 +0800

    增加了部分用的多的swift bat 解析，准备测试

--- src/gcn/GcnMailNoticeConverter.java
+++ src/gcn/GcnMailNoticeConverter.java
@@ -5,7 +5,9 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.configuration2.XMLConfiguration;
 import org.joda.time.Chronology;
@@ -14,6 +16,7 @@ import org.joda.time.DateTimeUtils;
 import org.joda.time.chrono.GJChronology;
 
 import com.mhuss.AstroLib.AstroDate;
+import com.sun.mail.handlers.image_gif;
 
 import jsky.coords.WorldCoords;
 import util.XML2File;
@@ -36,6 +39,7 @@ public class GcnMailNoticeConverter {
 		List<Object> whatValueList=config.getList("What.Param[@value]");	
 		List<Object> Trigger_ID_groupValue=config.getList("What.Group(0).Param[@value]");	
 		List<Object> Obs_groupValue=config.getList("What.Group(2).Param[@value]");	 //Obs_Support_Info_groupValue
+		List<Object> Obs_groupName=config.getList("What.Group(2).Param[@name]");	 //Obs_Support_Info_groupName
 
 //		List<Object> whatGroup=config.get("What.Group[@name]");	
 //		String TrigID = config.getString("What.Param.Pkt_Ser_Num.value");
@@ -428,6 +432,7 @@ public class GcnMailNoticeConverter {
 			messageContent = convertXmlToGcnNotice_Swift_Alert( config, gcnTitle_1 , typeNumber);
 			break;
 		case SWIFT_BAT_GRB_POS_ACK:
+			messageContent = convertXmlToGcnNotice_Swift_Pos_ACK( config, gcnTitle_1 , typeNumber);
 			break;
 		case SWIFT_BAT_GRB_LC:
 			break;
@@ -541,7 +546,163 @@ public class GcnMailNoticeConverter {
 	}
 	
 	
+	static String convertXmlToGcnNotice_Swift_Pos_ACK(XMLConfiguration config,String gcnTitle_1 ,int typeNumber){
+		String Tab3append="                ";
+		String Tab1append="  ";
+		String NewLineAppend= "<br/>";
+		String preHead= "<pre>";
+		String preEnd= "</pre>";
+		String slashSep= "///////////////////////////////////////////////////////////";
+		StringBuilder gcnNoticeStringBuilder=new StringBuilder();
+		
+		List<Object> whatNameList = config.getList("What.Param[@name]");
+		List<Object> whatValueList=config.getList("What.Param[@value]");	
+		List<Object> Trigger_ID_groupValue=config.getList("What.Group(0).Param[@value]");	
+		List<Object> Obs_groupValue=config.getList("What.Group(3).Param[@value]");	 //Obs_Support_Info_groupValue
+		List<Object> Obs_groupName=config.getList("What.Group(3).Param[@name]");	 //Obs_Support_Info_groupName
+		
+		String TrigID="无TrigID ";		
+		try {
+			TrigID=(String) whatValueList.get(whatNameList.indexOf("TrigID"));
+		} catch (Exception e) {
+			XML2File.writeToLog(e.getMessage());
+		}	
+		String Segment_Num="无Segment_Num ";		
+		try {
+			Segment_Num=(String) whatValueList.get(whatNameList.indexOf("Segment_Num"));
+		} catch (Exception e) {
+			XML2File.writeToLog(e.getMessage());
+		}	
+		
+		
+		String Trig_Index="无Trig_Index ";
+		try {
+			Trig_Index=(String) whatValueList.get(whatNameList.indexOf("Trig_Index"));
+		} catch (Exception e) {
+			XML2File.writeToLog(e.getMessage());
+		}	
+		String [] GRB_DateTime=getTJD_SOD(whatNameList,whatValueList);
+		
+		String Burst_Inten="--";
+		String Bkg_Inten="--";
+		String Bkg_Time="--";
+		String Bkg_Dur="--";
+		String Phi="--";
+		String Theta="--";
+		String Soln_Status="--";
+		String Image_Signif="--";
+		String MERIT_PARAMS="--";
+		try {
+			Burst_Inten = (String) whatValueList.get(whatNameList.indexOf("Burst_Inten"));
+			Bkg_Inten = (String) whatValueList.get(whatNameList.indexOf("Bkg_Inten"));
+			Bkg_Time = (String) whatValueList.get(whatNameList.indexOf("Bkg_Time"));
+			Bkg_Dur = (String) whatValueList.get(whatNameList.indexOf("Bkg_Dur"));
+			Phi = (String) whatValueList.get(whatNameList.indexOf("Phi"));
+			Theta = (String) whatValueList.get(whatNameList.indexOf("Theta"));
+			Soln_Status = (String) whatValueList.get(whatNameList.indexOf("Soln_Status"))+" 需要转换";
+			Image_Signif = (String) whatValueList.get(whatNameList.indexOf("Image_Signif"));
+			List<Object> Merit_groupValue=config.getList("What.Group(0).Param[@value]");
+			MERIT_PARAMS=getMERIT_PARAMS(Merit_groupValue);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		String NOTICE_DATE = config.getString("Who.Date");
+		String NOTICE_TYPE= Trigger_ID_groupValue.get(0).toString();
+		
+		NOTICE_TYPE=NOTICE_TYPE.equals("coinc")?"COINCIDENCE ???":NOTICE_TYPE;
+		NOTICE_TYPE=NOTICE_TYPE.equals("indiv")?"INDIVIDUAL ???样例中无INDIVIDUAL缩写":NOTICE_TYPE; //???? not sure about indiv 
+		
+		String EVENT_FLUENCE="无EVENT_FLUENCE "; //EVENT_FLUENCE	 is	Event_Signal
+		if (whatNameList.indexOf("Event_Signal")>=0) {
+			EVENT_FLUENCE=(String) whatValueList.get(whatNameList.indexOf("Event_Signal"));	
+		}else {		}
+		EVENT_FLUENCE= EVENT_FLUENCE+ "[neutrinos]"+"???";
+		
+		Map<String,String> raDec=getRaDec(config);
+		String Error2Radius= config.getString("WhereWhen.ObsDataLocation.ObservationLocation.AstroCoords.Position2D.Error2Radius","--");
+		String Rate_Signif="---";
+		try {
+			Rate_Signif = (String) whatValueList.get(whatNameList.indexOf("Rate_Signif"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String COMMENTS_1 = "部分信息可能不是从XML中获取到的";
+		
+		gcnNoticeStringBuilder.append(preHead);
+		gcnNoticeStringBuilder.append(slashSep).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("TITLE         :").append(Tab3append).append("GCN/").append(TYPE_MACRO_NAME.valueOf(typeNumber)).append("\tNotice ").append(NewLineAppend);
+		gcnNoticeStringBuilder.append("NOTICE_DATE   :").append(Tab3append).append(NOTICE_DATE).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("NOTICE_TYPE   :").append(Tab3append).append(TYPE_MACRO_NAME.valueOf(typeNumber)).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("TRIGGER_NUM   :").append(Tab3append).append(TrigID).append(Tab1append).append("Seg_Num : ").append(Segment_Num).append(NewLineAppend);
+		
+		gcnNoticeStringBuilder.append("GRB_RA        :").append(Tab3append).append(raDec.get("Event_Ra_J2000")   ).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("              :").append(Tab3append).append(raDec.get("Event_Ra_Current") ).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("              :").append(Tab3append).append(raDec.get("Event_Ra_J1950")   ).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("GRB_DEC       :").append(Tab3append).append(raDec.get("Event_Dec_J2000")  ).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("              :").append(Tab3append).append(raDec.get("Event_Dec_Current")).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("              :").append(Tab3append).append(raDec.get("Event_Dec_J1950")  ).append(NewLineAppend);
+		
+		gcnNoticeStringBuilder.append("GRB_ERROR      :").append(Tab3append).append(Error2Radius).append("???").append(NewLineAppend);
+		gcnNoticeStringBuilder.append("GRB_INTEN      :").append(Tab3append).append(Burst_Inten).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("BKG_INTEN      :").append(Tab3append).append(Bkg_Inten).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("BKG_TIME       :").append(Tab3append).append(Bkg_Time).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("BKG_DUR        :").append(Tab3append).append(Bkg_Dur).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("GRB_DATE       :").append(Tab3append).append(GRB_DateTime[0]).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("GRB_TIME       :").append(Tab3append).append(GRB_DateTime[1]).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("北京时间        :").append(Tab3append).append(GRB_DateTime[2]).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("GRB_PHI        :").append(Tab3append).append(Phi).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("GRB_THETA      :").append(Tab3append).append(Theta).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("TRIGGER_INDEX  :").append(Tab3append).append(Trig_Index).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("SOLN_STATUS    :").append(Tab3append).append(Soln_Status).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("RATE_SIGNIF    :").append(Tab3append).append(Rate_Signif).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("IMAGE_SIGNIF   :").append(Tab3append).append(Image_Signif).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("MERIT_PARAMS   :").append(Tab3append).append(MERIT_PARAMS).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("SUN_POSTN      :").append(Tab3append).append(getSUN_POSTN(Obs_groupValue, Obs_groupName)).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("SUN_DIST       :").append(Tab3append).append(getSUN_POSTN(Obs_groupValue, Obs_groupName)).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("MOON_POSTN     :").append(Tab3append).append(getMOON_POSTN(Obs_groupValue, Obs_groupName)).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("MOON_DIST      :").append(Tab3append).append(getMOON_DIST(Obs_groupValue, Obs_groupName)).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("GAL_COORDS     :").append(Tab3append).append(getGAL_COORDS(Obs_groupValue, Obs_groupName)).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("ECL_COORDS     :").append(Tab3append).append(getECL_COORDS(Obs_groupValue, Obs_groupName)).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("COMMENTS       :").append(Tab3append).append(COMMENTS_1).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("COMMENTS       :").append(Tab3append).append(COMMENTS_1).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("COMMENTS       :").append(Tab3append).append(COMMENTS_1).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("COMMENTS       :").append(Tab3append).append(COMMENTS_1).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("COMMENTS       :").append(Tab3append).append(COMMENTS_1).append(NewLineAppend);
+		
+		
+//		gcnNoticeStringBuilder.append(getTJD_SOD(whatNameList,whatValueList));
+		
+		gcnNoticeStringBuilder.append("TRIGGER_INDEX:").append(Tab3append).append(Trig_Index).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("Rate_Signif  :").append(Tab3append).append(Rate_Signif).append(NewLineAppend);
+		gcnNoticeStringBuilder.append("COMMENTS     :").append(Tab3append).append(COMMENTS_1).append(NewLineAppend);
+//		gcnNoticeStringBuilder.append("北京时间:").append(Tab3append).append("__").append(NewLineAppend);
+		gcnNoticeStringBuilder.append(slashSep).append(NewLineAppend);
+		gcnNoticeStringBuilder.append(preEnd);
+//		gcnNoticeStringBuilder.append("</table >");
+		
+		String stringRa = config.getString("WhereWhen.ObsDataLocation.ObservationLocation.AstroCoords.Position2D.Value2.C1");
+		
+		
+//		System.out.println("///////////////////////////////////////////////////////////");
+		System.out.println(gcnNoticeStringBuilder.toString());
+//		System.out.println("///////////////////////////////////////////////////////////");
+		
+		return gcnNoticeStringBuilder.toString();
+	}
 	
+	
+	
+	private static String getMERIT_PARAMS(List<Object> merit_groupValue) {
+		String result="";
+		for (Object object : merit_groupValue) {
+			result=result+object.toString();
+		}
+		return result;
+	}
 	public static String[] getTJD_SOD(List<Object> whatNameList,List<Object> whatValueList){
 		String []EVENT_TIME=new String[3];
 		try {
@@ -584,6 +745,122 @@ public class GcnMailNoticeConverter {
 		return EVENT_TIME;
 	}
 	
+	
+	static Map<String, String> getRaDec(XMLConfiguration config){
+		Map <String,String> resultMap=new HashMap<String, String>();
+		String Event_Ra_J2000="Undefined"+"\t(J2000)";
+		String Event_Ra_Current="Undefined"+"\t(current)";
+		String Event_Ra_J1950="Undefined"+"\t(1950)";
+		String Event_Dec_J2000="Undefined"+"\t(J2000)";
+		String Event_Dec_Current="Undefined"+"\t(current)";
+		String Event_Dec_J1950="Undefined"+"\t(1950)";
+		 
+		String EVENT_RA = config.getString("WhereWhen.ObsDataLocation.ObservationLocation.AstroCoords.Position2D.Value2.C1");
+		String EVENT_DEC = config.getString("WhereWhen.ObsDataLocation.ObservationLocation.AstroCoords.Position2D.Value2.C2");
+		Calendar caNow=Calendar.getInstance();
+		WorldCoords wcRaDec=new WorldCoords(Double.parseDouble(EVENT_RA), Double.parseDouble(EVENT_DEC),2000.);
+		double[] arCurrent= wcRaDec.getRaDec(caNow.get(Calendar.YEAR));
+		double[] ar1950= wcRaDec.getRaDec(1950);
+		WorldCoords wcRaDecCurrent = new WorldCoords(arCurrent[0],arCurrent[1]);
+		WorldCoords wcRaDec1950 = new WorldCoords(ar1950[0], ar1950[1]);
+		
+		
+		 Event_Ra_J2000=wcRaDec.getRaDeg()+"d\t{"+wcRaDec.getRA().toString()+"}\t(J2000)";
+		 Event_Ra_Current=wcRaDecCurrent.getRaDeg()+"d\t{"+wcRaDecCurrent.getRA().toString()+"}\t(current)";
+		 Event_Ra_J1950=wcRaDec1950.getRaDeg()+"d\t{"+wcRaDec1950.getRA().toString()+"}\t(1950)";
+		
+		 Event_Dec_J2000=wcRaDec.getDecDeg()+"d\t{"+wcRaDec.getDec().toString()+"}\t(J2000)";
+		 Event_Dec_Current=wcRaDecCurrent.getDecDeg()+"d\t{"+wcRaDecCurrent.getDec().toString()+"}\t(current)";
+		 Event_Dec_J1950=wcRaDec1950.getDecDeg()+"d\t{"+wcRaDec1950.getDec().toString()+"}\t(1950)";
+			
+		 resultMap.put("Event_Ra_J2000", Event_Ra_J2000);
+		 resultMap.put("Event_Ra_Current", Event_Ra_Current);
+		 resultMap.put("Event_Ra_J1950", Event_Ra_J1950);
+		 resultMap.put("Event_Dec_J2000", Event_Dec_J2000);
+		 resultMap.put("Event_Dec_Current", Event_Dec_Current);
+		 resultMap.put("Event_Dec_J1950", Event_Dec_J1950);
+		return resultMap;
+		 
+	}
 
+	static String getSUN_POSTN(List<Object> Obs_groupValue,List<Object> Obs_groupName){
+		try {
+			String Sun_RA = Obs_groupValue.get(Obs_groupName.indexOf("Sun_RA")).toString();
+			String Sun_DEC = Obs_groupValue.get(Obs_groupName.indexOf("Sun_Dec")).toString();
+			WorldCoords sun_wcRaDec=new WorldCoords(Double.parseDouble(Sun_RA), Double.parseDouble(Sun_DEC),2000.);
+			return  sun_wcRaDec.getRaDeg()+"d {"+sun_wcRaDec.getRA()+"}    "+sun_wcRaDec.getDecDeg() +"d {"+sun_wcRaDec.getDec()+"}";
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "--";
+		}
+	}
+	static String getSUN_DIST(List<Object> Obs_groupValue,List<Object> Obs_groupName){
+		try {
+			return Obs_groupValue.get(Obs_groupName.indexOf("Sun_Distance")).toString();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "--";
+		}
+
+	}
+	static String getMOON_POSTN(List<Object> Obs_groupValue,List<Object> Obs_groupName){
+		try {
+			String Moon_RA = Obs_groupValue.get(4).toString();
+			String Moon_DEC = Obs_groupValue.get(5).toString();
+			WorldCoords moon_wcRaDec=new WorldCoords(Double.parseDouble(Moon_RA), Double.parseDouble(Moon_DEC),2000.);
+			return moon_wcRaDec.getRaDeg()+"d {"+moon_wcRaDec.getRA()+"}    "+moon_wcRaDec.getDecDeg() +"d {"+moon_wcRaDec.getDec()+"}";
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "--";
+		}
+		
+	}
+	static String getMOON_DIST(List<Object> Obs_groupValue,List<Object> Obs_groupName){
+		try {
+			return Obs_groupValue.get(Obs_groupName.indexOf("MOON_Distance")).toString();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "--";
+		}
+	}
+
+	static String getMoon_Illum(List<Object> Obs_groupValue,List<Object> Obs_groupName){
+		try {
+			return Obs_groupValue.get(Obs_groupName.indexOf("Moon_Illum")).toString();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "--";
+		}
+	}
+	static String getGAL_COORDS(List<Object> Obs_groupValue,List<Object> Obs_groupName){
+		try {
+			String Galactic_Long = Obs_groupValue.get(Obs_groupName.indexOf("Galactic_Long")).toString();
+			String Galactic_Lat = Obs_groupValue.get(Obs_groupName.indexOf("Galactic_Lat")).toString();
+			return Galactic_Long+","+Galactic_Lat+ " [deg] galactic lon,lat of the event";
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "--";
+		}
+	}
+	static String getECL_COORDS(List<Object> Obs_groupValue,List<Object> Obs_groupName){
+		try {
+			String Ecliptic_Long = Obs_groupValue.get(Obs_groupName.indexOf("Ecliptic_Long")).toString();
+			String Ecliptic_Lat = Obs_groupValue.get(Obs_groupName.indexOf("Ecliptic_Lat")).toString();
+			return Ecliptic_Long+","+Ecliptic_Lat+ " [deg] ecliptic lon,lat of the event";
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "--";
+		}
+		
+	}
+	
+	
 
 }
